49b2755c79844f55444f115a69120714d2074765
HusbanTech12 <husbantech08@gmail.com>
2024-12-15 17:47:37 +0500

    add functionality

--- src/app/components/Task-Manager.tsx
+++ src/app/components/Task-Manager.tsx
@@ -1,25 +1,28 @@
 
 'use client';
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 
 const TaskManager = () => {
 
-    const [tasks, setTasks] = useState()
+    const [tasks, setTasks] = useState([])
     
+    const inputReference = useRef()
+    
+
+
     const handleAddTask = () => {
-        console.log('Clicked');
+    
+        const inputValue = inputReference.current.value
+        setTasks([inputValue , ...tasks])
+        
+      
         
 
     }
-
-
-
-
-
-
-
+    
+        
 
 
 
@@ -28,14 +31,22 @@ const TaskManager = () => {
 
         <>
         <div className="ml-14 mt-10">
-                <input type="text" placeholder="Add Task"  className="bg-slate-600 h-10 text-green-300"/>
+                <input
+                    ref={inputReference} type="text" placeholder="Add Task"
+                    className="bg-slate-600 h-10 text-green-300" />
           <button   onClick={handleAddTask}    className="w-32 rounded-md h-12 bg-blue-500 text-white ml-10">Add Task</button>      
 
 
 
         </div>
         
-        
+
+            <ol>
+                {tasks.map((elem) => {
+                    return <li key={elem}>{elem}</li>
+                    
+            })}
+            </ol>           
         
         </>
 
