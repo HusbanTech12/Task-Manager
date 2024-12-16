3344c266fef41146913ed39db02937a6199ba6a4
HusbanTech12 <husbantech08@gmail.com>
2024-12-17 00:17:55 +0500

    add funtion handleOnKeyDown

--- src/app/components/Task-Manager.tsx
+++ src/app/components/Task-Manager.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useRef, useState } from "react"
-
+import { nanoid } from 'nanoid';
 
 const TaskManager = () => {
 
@@ -15,10 +15,25 @@ const TaskManager = () => {
     const handleAddTask = () => {
     
         const inputValue = inputReference.current.value
-        setTasks([inputValue , ...tasks])
-        
+        setTasks([{title: inputValue , id: nanoid()} , ...tasks])
+        if (inputReference.current) {
+            inputReference.current.value = '';
+        }
       
         
+
+    }
+
+
+    const handleOnKeyDown = (My_key) : void => {
+
+        console.log(My_key.key.value);
+        
+        if (My_key.key === 'h' || My_key.key === 'Enter') {
+            handleAddTask()
+        }
+    
+
 
     }
     
@@ -32,6 +47,7 @@ const TaskManager = () => {
         <>
         <div className="ml-14 mt-10">
                 <input
+                    onKeyDown={handleOnKeyDown}
                     ref={inputReference} type="text" placeholder="Add Task"
                     className="bg-slate-600 h-10 text-green-300" />
           <button   onClick={handleAddTask}    className="w-32 rounded-md h-12 bg-blue-500 text-white ml-10">Add Task</button>      
@@ -43,7 +59,7 @@ const TaskManager = () => {
 
             <ol>
                 {tasks.map((elem) => {
-                    return <li key={elem}>{elem}</li>
+                    return <li key={elem.id}>{elem.title}</li>
                     
             })}
             </ol>           
